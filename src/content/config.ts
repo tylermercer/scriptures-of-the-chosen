1f77254497ea2ffb78b0d25a7670ec036910da9c
Tyler Mercer <acct@tylermercer.net>
2024-09-23 20:22:31 -0600

    Consolidate loaders into content config to fix bug with types
    Allows me to remove the type casts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
-import { z, defineCollection } from "astro:content";
-import episodesLoader from "src/loaders/episodes";
+import { z, defineCollection, reference } from "astro:content";
 
 if (!import.meta.env.GOOGLE_SHEET_APP_URL.startsWith('https')) {
   throw new Error("GOOGLE_SHEET_APP_URL not set at build time")
@@ -25,7 +24,37 @@ const seasons = defineCollection({
 
 const episodes = defineCollection({
   type: 'content_layer',
-  loader: episodesLoader,
+  loader: async () => {
+    const references = await fetch(`${import.meta.env.GOOGLE_SHEET_APP_URL}?s=References`)
+      .then(r => r.json())
+      .then(r => r.filter((e: any) => e.episode && e.season));
+
+    const episodes = await fetch(`${import.meta.env.GOOGLE_SHEET_APP_URL}?s=Episodes`)
+      .then(r => r.json())
+      .then(r => r
+        .filter((e: any) => e.season && e.episode)
+        .sort((e: any) => e.season + e.episode)
+      );
+
+    return episodes.map((e: any) => ({
+      id: `${e.season}:${e.episode}`,
+      ...e,
+      references: references.filter((r: any) => r.season === e.season && r.episode === e.episode)
+    }));
+  },
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    season: reference('seasons'),
+    episode: z.string(),
+    watchUrl: z.string().url(),
+    references: z.array(z.object({
+      timeRange: z.string().optional(),
+      notes: z.string(),
+      passage: z.string(),
+      type: z.string(),
+    }))
+  }),
 });
 
 export const collections = {
